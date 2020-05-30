5e0fe1b21fe24538eb8dbbecfc72e2d367aab1a7
Balu <>
2020-05-30 17:21:05 +0200

    And it works, like a magic and passes all 3 test sets. :) I overthought it at first, like keeping track of the cat where it moved and what its next step will be, and who could I get there or should I stay etc.. Instead, it was plain simple: 1.) Cat moved somewhere 2.) calculate the distance from my (0,0) origo point. 3.) Is our distance greater than the steps the cat did already? True -> continue False -> I got the number for the test case

--- src/com/google/codejam/year2020/round1C/overexcitedFan01/Solution.java
+++ src/com/google/codejam/year2020/round1C/overexcitedFan01/Solution.java
@@ -110,8 +110,8 @@ Test inputs:
 @see https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef4/0000000000317409
 */
 public class Solution {
-    private static final boolean LOCAL_TESTING = false;
-    private static final boolean LOCAL_TESTING_SOLUTION = false;
+    private static final boolean LOCAL_TESTING = true;
+    private static final boolean LOCAL_TESTING_SOLUTION = true;
     private static final String OUTPUT_FORMAT = "Case #%d: %s"; //Use with String.format - 1.: number of the test case, 2.: some string
     private static final String SEPARATOR = " ";
     //the directions
@@ -134,231 +134,105 @@ test:   for (int currentTestCase = 1; currentTestCase <= T; currentTestCase++) {
             final int Y = in.nextInt(); //y blocks NORTH away from my start position
             final String CAT_PATH = in.nextLine().trim();
             StringBuilder sb = new StringBuilder();
-            int walkCounter = 0; //how many blocks we had to walk. 1 block = 1 minutes
-            /*
-            //Test set 1:
-            boolean testSet1 = 
-                    (X <= 10) &&
-                    (Y <= 10) &&
-                    (CAT_PATH.length() <= 8) &&
-                    (!(CAT_PATH.contains(E+"") || CAT_PATH.contains(W+"")))
-            ;
-            boolean testSet2 = 
-                    !testSet1 && 
-                    (X <= 1000) &&
-                    (Y <= 1000) &&
-                    (CAT_PATH.length() <= 1000) &&
-                    (!(CAT_PATH.contains(E+"") || CAT_PATH.contains(W+"")))
-            ;
-            boolean testSet3 = !(testSet1 || testSet2);
-            */
-            boolean CAT_WALKS_ONLY_VERTICAL = !(CAT_PATH.contains(E+"") || CAT_PATH.contains(W+""));
-            //I am starting from 0,0. The cat starts east from me and north from me
-            int catX = (-1) * X;
-            int catY = (-1) * Y;
-            int catEndX = catX;
-            int catEndY = catY;
+            int myCurrentX = 0;
+            int myCurrentY = 0;
+            int catCurrentX = goEast() * X; //I take my starting point as (0,0), so east is -1*X away
+            int catCurrentY = goNorth() * Y; //I take my starting point as (0,0), so north is +1*Y away
+            //lets calculate for every step of the cat, how long would it take for me to get there
             for(int i=0; i<CAT_PATH.length(); i++) {
-                char direction = CAT_PATH.charAt(i);
-                switch(direction) {
-                    case N: {
-                        catEndY -= 1;
-                        break;
-                    }
-                    case S: {
-                        catEndY += 1;
-                        break;
-                    }
-                    case E: {
-                        catEndX -= 1;
-                        break;
-                    }
-                    case W: {
-                        catEndX += 1;
-                        break;
-                    }
+                char catCurrentStepWillBe = CAT_PATH.charAt(i);
+                //after doing cat"s current step, cat ends up at
+                catCurrentX += getNextStepVectorForHorizontalDirection(catCurrentStepWillBe);
+                catCurrentY += getNextStepVectorForVerticalDirection(catCurrentStepWillBe);
+                //cat's current position after doing the current step is at (catCurrentX,catCurrentY)
+                int catWalkCounter = i+1; //how many blocks did the cat walk after doing the current step
+                //How long would it take for me to get there?
+                int myWalkCounter = 0; //how many blocks we had to walk. 1 block = 1 minutes
+                int currentStepsNeededBetweenMeAndCat = Math.abs(catCurrentX - myCurrentX) + Math.abs(catCurrentY - myCurrentY);
+                if(catWalkCounter < currentStepsNeededBetweenMeAndCat) {
+                    //we cant get there in time, lets move on to the next cat step
+                    continue;
                 }
+                //we can reach this station within the steps the cat did so far
+                //since we don't even need my path to get there, to finish this test case, it is enough to know, how many steps the cat did so far
+                sb.append(catWalkCounter);
+                break;
             }
-            testPrintln("From (" + X + "," + Y + ") that is (" + catX + "," + catY + ") the path '" + CAT_PATH + "' lead to (" + catEndX + "," + catEndY + ").");
-            int CAT_END_AWAY_IN_MINUTES = Math.abs(catEndX) + Math.abs(catEndY);
-            final int CAT_TOUR_LENGTH_IN_MINUTES = CAT_PATH.length();
-            testPrintln("End of tour is in " + CAT_END_AWAY_IN_MINUTES + " blocks/minutes away from me.");
-            if(false && CAT_TOUR_LENGTH_IN_MINUTES < CAT_END_AWAY_IN_MINUTES) {
-                sb.append(IMPOSSIBLE);
+            
+            //print solution for current test case
+            if(sb.length() == 0) {
+                printSolution((X+SEPARATOR+Y+SEPARATOR+CAT_PATH), currentTestCase, IMPOSSIBLE);
             }
-            else { 
-                int catMoveX = 0;
-                int catMoveY = 0;
-                int catMoveNextX = 0;
-                int catMoveNextY = 0;
-                int myX = 0;
-                int myY = 0;
-steps:          for(int i=0; i<CAT_PATH.length(); i++) {
-                    char direction = CAT_PATH.charAt(i);
-                    Character nextDirection = (i+1 < CAT_PATH.length()) ? CAT_PATH.charAt(i+1) : null;
-                    switch(direction) {
-                        case N: {
-                            catMoveX = 0;
-                            catMoveY = -1;
-                            break;
-                        }
-                        case S: {
-                            catMoveX = 0;
-                            catMoveY = 1;
-                            break;
-                        }
-                        case E: {
-                            catMoveX = -1;
-                            catMoveY = 0;
-                            break;
-                        }
-                        case W: {
-                            catMoveX = 1;
-                            catMoveY = 0;
-                            break;
-                        }
-                    }
-                    if(nextDirection == null) {
-                        catMoveNextX = 0;
-                        catMoveNextY = 0;
-                    }
-                    else {
-                        switch(nextDirection) {
-                            case N: {
-                                catMoveNextY = -1;
-                                break;
-                            }
-                            case S: {
-                                catMoveNextY = 1;
-                                break;
-                            }
-                            case E: {
-                                catMoveNextX = -1;
-                                break;
-                            }
-                            case W: {
-                                catMoveNextX = 1;
-                                break;
-                            }
-                        }
-                    }
-                    //move myself ()neither me, nor the cat has moved in this step yet. Should I wait or should I go?
-                    int diffDirectionX = myX - catX;
-                    int diffDirectionY = myY - catY;
-                    int diffX = Math.abs(diffDirectionX);
-                    int diffY = Math.abs(diffDirectionY);
-                    if(myX != catX) { //if are X did not meet yet, we go this way and only after in Y direction
-                        if(2 <= diffX) { //I can move towards the cat, we wont cross within 1 step
-                            if(0 < diffDirectionX) { //to the EAST from me: 0--4 || -1--4 || 3--4
-                                myX += -1;
-                            }
-                            else if(diffDirectionX == 0) { //stay: -4--4 || 3-3
-                                myX += 0;
-                                throw new RuntimeException("Can't happen, or it means bug in my logical solution");
-                            }
-                            else {
-                                myX += 1;
-                            }
-                        }
-                        else if(1 == diffX) { //I need to check if its next step comes back to me and I should rather not move or move differently
-                            String catsPathReminder = CAT_PATH.substring(i, CAT_PATH.length());
-                            boolean willstillComeTowardsMe = false;
-                            if(0 < diffDirectionX && catsPathReminder.contains(W+"")) { //if cat is to the east from me, but will still come to the west some time
-                                willstillComeTowardsMe = true;
-                            }
-                            else if(diffDirectionX < 0 && catsPathReminder.contains(E+"")) { //if cat is to the west from me, but will still come to the east some time
-                                willstillComeTowardsMe = true;
-                            }
-                            if(willstillComeTowardsMe) { //it will come towards me, lets move rather vertically with it
-                                myY += catMoveY; //I move with the cat
-                            }
-                            else { //wont come towards me
-                                int catPozNewX = catX + catMoveX;
-                                if(catPozNewX == myX) { //I should not move, cat comes back
-                                    myX += 0;
-                                }
-                                else {
-                                    if(0 < diffDirectionX) { //to the EAST from me: 0--4 || -1--4 || 3--4
-                                        myX += -1;
-                                    }
-                                    else if(diffDirectionX == 0) { //stay: -4--4 || 3-3
-                                        myX += 0;
-                                        throw new RuntimeException("Can't happen, or it means bug in my logical solution");
-                                    }
-                                    else {
-                                        myX += 1;
-                                    }
-                                }
-                            }
-                        }
-                        else {
-                            //myX == catX, can't come here, because we go vertical instead
-                        }
-                    }
-                    else { //our X is the same, move towards Y
-                        if(2 <= diffY) { //I can move towards the cat, we wont cross within 1 step
-                            if(0 < diffDirectionY) { //to the NORTH from me: 0--4 || -1--4 || 3--4
-                                myY += -1;
-                            }
-                            else if(diffDirectionY == 0) { //stay: -4--4 || 3-3
-                                myY += 0;
-                                //we reached the cat's position
-                            }
-                            else {
-                                myY += 1;
-                            }
-                        }
-                        else if(1 == diffY) { //I need to check if its next step comes back to me and I should rather not move
-                            String catsPathReminder = CAT_PATH.substring(i, CAT_PATH.length());
-                            boolean willstillComeTowardsMe = false;
-                            if(0 < diffDirectionY && catsPathReminder.contains(S+"")) { //if cat is to the north from me, but will still come to the south some time
-                                willstillComeTowardsMe = true;
-                            }
-                            else if(diffDirectionY < 0 && catsPathReminder.contains(N+"")) { //if cat is to the south from me, but will still come to the north some time
-                                willstillComeTowardsMe = true;
-                            }
-                            if(willstillComeTowardsMe) { //it will come towards me, lets move rather horizontally with it
-                                myX += catMoveX; //I move with the cat
-                            }
-                            else { //wont come towards me
-                                int catPozNewY = catY + catMoveY;
-                                if(catPozNewY == myY) { //I should not move, cat comes back
-                                    myY += 0;
-                                }
-                                else {
-                                    if(0 < diffDirectionY) { //to the NORTH from me: 0--4 || -1--4 || 3--4
-                                        myY += -1;
-                                    }
-                                    else if(diffDirectionY == 0) { //stay: -4--4 || 3-3
-                                        myY += 0;
-                                        //we reached the cat's position
-                                    }
-                                    else {
-                                        myY += 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    
-                    //move the cat according to its path
-                    catX += catMoveX;
-                    catY += catMoveY;
-                    
-                    testPrintln("Cat moved to (" + catX + "," + catY + ") and I moved to (" + myX + "," + myY + ").");
-                    walkCounter++;
-                    if(catX == myX && catY == myY) { //after moving, if at any step we meet, I am done
-                        sb.append(walkCounter);
-                        printSolution((X+SEPARATOR+Y+SEPARATOR+CAT_PATH), currentTestCase, sb.toString());
-                        continue test;
-                    }
-                } //end of cat tour cyclus
-                sb.append(IMPOSSIBLE);
+            else {
+                //System.out.println(String.format(OUTPUT_FORMAT, currentTestCase, "TEST"));
+                printSolution((X+SEPARATOR+Y+SEPARATOR+CAT_PATH), currentTestCase, sb.toString());
             }
-            //System.out.println(String.format(OUTPUT_FORMAT, currentTestCase, "TEST"));
-            printSolution((X+SEPARATOR+Y+SEPARATOR+CAT_PATH), currentTestCase, sb.toString());
         } //end of test cases
     } //end of main
+    
+    protected static int goEast() {
+        return getNextStepVectorForDirection(E);
+    }
+    protected static int goWest() {
+        return getNextStepVectorForDirection(W);
+    }
+    protected static int goNorth() {
+        return getNextStepVectorForDirection(N);
+    }
+    protected static int goSouth() {
+        return getNextStepVectorForDirection(S);
+    }
+    protected static int getNextStepVectorForHorizontalDirection(char directionToGoNext) {
+        switch(directionToGoNext) {
+            case E: {
+                return goEast();
+            }
+            case W: {
+                return goWest();
+            }
+            default: {
+                return 0; //this step was not horizontal
+            }
+        }
+    }
+    protected static int getNextStepVectorForVerticalDirection(char directionToGoNext) {
+        switch(directionToGoNext) {
+            case N: {
+                return goNorth();
+            }
+            case S: {
+                return goSouth();
+            }
+            default: {
+                return 0; //this step was not vertical
+            }
+        }
+    }
+    /**
+     * Given a valid direction, it gives back the direction vector for it. It is just like a coordinate-system from math class.
+     * 
+     * @param directionToGoNext
+     * @return 
+     */
+    private static int getNextStepVectorForDirection(char directionToGoNext) {
+        switch(directionToGoNext) {
+            case E: {
+                return -1;
+            }
+            case W: {
+                return 1;
+            }
+            case N: {
+                return 1;
+            }
+            case S: {
+                return -1;
+            }
+            default: {
+                throw new RuntimeException("Wrong direction: " + directionToGoNext);
+            }
+        }
+    }
     
     /**
      * Helper method to print the solution and locally to test the expected values.
@@ -380,11 +254,11 @@ steps:          for(int i=0; i<CAT_PATH.length(); i++) {
                 testPrint(" <-- " + (expected.equals(solution) ? "OK" : "NOT OK") + " | '" + expected + "' was expected for input: " + input);
             }
             else if("3 0 SNSS".equals(input)) {
-                expected = "IMPOSSIBLE";
+                expected = IMPOSSIBLE;
                 testPrint(" <-- " + (expected.equals(solution) ? "OK" : "NOT OK") + " | '" + expected + "' was expected for input: " + input);
             }
             else if("2 10 NSNNSN".equals(input)) {
-                expected = "IMPOSSIBLE";
+                expected = IMPOSSIBLE;
                 testPrint(" <-- " + (expected.equals(solution) ? "OK" : "NOT OK") + " | '" + expected + "' was expected for input: " + input);
             }
             else if("0 1 S".equals(input)) {
@@ -413,8 +287,9 @@ steps:          for(int i=0; i<CAT_PATH.length(); i++) {
      */
     protected static void testPrintln(String s) {
         if(LOCAL_TESTING) {
-            System.err.println(s);
-            System.err.flush();
+            System.out.println(s);
+            //System.err.println(s);
+            //System.err.flush();
         }
     }
     /**
@@ -423,8 +298,9 @@ steps:          for(int i=0; i<CAT_PATH.length(); i++) {
      */
     protected static void testPrint(String s) {
         if(LOCAL_TESTING) {
-            System.err.print(s); 
-            System.err.flush();
+            System.out.print(s); 
+            //System.err.print(s); 
+            //System.err.flush();
         }
     }
 }
